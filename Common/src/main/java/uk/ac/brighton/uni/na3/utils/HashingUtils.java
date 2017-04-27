9944800b40a87e03503cd0046a6f10e427ab22c3
ch629 <zurrox@live.com>
2017-04-27 15:01:21 +0100

    Utils class.

---
+++ Common/src/main/java/uk/ac/brighton/uni/na3/utils/HashingUtils.java
@@ -0,0 +1,10 @@
+package uk.ac.brighton.uni.na3.utils;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class HashingUtils {
+    public static char[] saltHash(char[] password) { //NOTE: The password is better as a char[] as it wont be stored in one location in RAM
+        //TODO: Ben - Implement (Rename this or add any methods as necessary)
+        throw new NotImplementedException();
+    }
+}
