9c890fae0778616b135bb3d3020f2edb6f1fcf47
Jabenjy <jabenjy@gmail.com>
2017-04-28 14:44:42 +0100

    start on HashUtils.java. Unfinished

--- Common/src/main/java/uk/ac/brighton/uni/na3/utils/HashingUtils.java
+++ Common/src/main/java/uk/ac/brighton/uni/na3/utils/HashingUtils.java
@@ -1,10 +1,39 @@
-package uk.ac.brighton.uni.na3.utils;
+//package uk.ac.brighton.uni.na3.utils;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+//import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class HashingUtils {
-    public static char[] saltHash(char[] password) { //NOTE: The password is better as a char[] as it wont be stored in one location in RAM
+    public static char[] saltHash(char[] password, boolean newPW) { //NOTE: The password is better as a char[] as it wont be stored in one location in RAM
         //TODO: Ben - Implement (Rename this or add any methods as necessary)
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        if (newPW == true) {
+          byte[] salt = genSalt();
+          char[] hSPassword = getHash(password, salt);
+        }
+        return password;
+    }
+
+    public static byte[] genSalt() {
+      byte[] salt = new byte[256];
+      Random randNo = new Random();
+      randNo.nextBytes(salt);
+      return salt;
+    }
+
+    public static char[] getHash(char[] password, byte[] salt) {
+      try {
+    	  MessageDigest md = MessageDigest.getInstance("SHA-256");
+      }catch (NoSuchAlgorithmException e){
+
+      }
+      int length = password.length + salt.length;
+      StringBuilder s = new StringBuilder(length);
+      for (char c : password) {
+    	  s.append(c);
+      }
+      return password;
     }
 }
