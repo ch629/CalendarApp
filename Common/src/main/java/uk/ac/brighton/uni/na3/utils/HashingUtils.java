7cf0bd6604f64dc895a957bd13aaa0291f4f09e5
Jabenjy <jabenjy@gmail.com>
2017-05-02 13:31:25 +0100

    has errors, and very untidy

--- Common/src/main/java/uk/ac/brighton/uni/na3/utils/HashingUtils.java
+++ Common/src/main/java/uk/ac/brighton/uni/na3/utils/HashingUtils.java
@@ -6,14 +6,18 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 public class HashingUtils {
-    public static char[] saltHash(char[] password, boolean newPW) { //NOTE: The password is better as a char[] as it wont be stored in one location in RAM
+
+    public static char[] saltHash(char[] password, boolean newPW, byte[] uSalt) { //NOTE: The password is better as a char[] as it wont be stored in one location in RAM
         //TODO: Ben - Implement (Rename this or add any methods as necessary)
         //throw new NotImplementedException();
         if (newPW == true) {
           byte[] salt = genSalt();
-          char[] hSPassword = getHash(password, salt);
+          byte[] hSPassword = getHash(password, salt);
+          return hSPassword.toString().toCharArray();
+        } else {
+        	byte[] hSPassword = getHash(password, uSalt);
+            return hSPassword.toString().toCharArray();
         }
-        return password;
     }
 
     public static byte[] genSalt() {
@@ -23,17 +27,22 @@ public class HashingUtils {
       return salt;
     }
 
-    public static char[] getHash(char[] password, byte[] salt) {
+    public static byte[] getHash(char[] password, byte[] salt) {
+
+      String temp = password.toString() + salt.toString();
+      byte[] pWChar = password.toString().getBytes();
+      byte[] pWSalt = new byte[pWChar.length + salt.length];
+      System.arraycopy(pWChar, 0, pWSalt, 0, pWChar.length);
+      System.arraycopy(salt, 0, pWSalt, pWChar.length, salt.length);
+
       try {
-    	  MessageDigest md = MessageDigest.getInstance("SHA-256");
+    	  MessageDigest md = MessageDigest.getInstance("SHA-1");
+    	  md.update(pWSalt, 0, pWSalt.length);
+    	  return md.digest(temp.getBytes());
       }catch (NoSuchAlgorithmException e){
 
       }
-      int length = password.length + salt.length;
-      StringBuilder s = new StringBuilder(length);
-      for (char c : password) {
-    	  s.append(c);
-      }
-      return password;
+
+      return pWChar;
     }
 }
