f9f856462071ba5548132db2e05c8aa6cebfa86e
Lewis-Allen <lraa12@brighton.ac.uk>
2017-05-04 15:35:49 +0100

    Added fxml controllers

---
+++ Client/src/main/java/uk/ac/brighton/uni/na3/javafx/createEventScreen/CreateEventController.java
@@ -0,0 +1,44 @@
+package uk.ac.brighton.uni.na3.javafx.createEventScreen;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CreateEventController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField timeField;
+
+    @FXML
+    private TextField locationField;
+
+    @FXML
+    private TextField durationField;
+
+    @FXML
+    private TextField attendeeField;
+
+    @FXML
+    private TextArea descriptionField;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    void cancelClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comfirmClicked(ActionEvent event) {
+
+    }
+
+}
---
+++ Client/src/main/java/uk/ac/brighton/uni/na3/javafx/editEventScreen/EditEventController.java
@@ -0,0 +1,30 @@
+package uk.ac.brighton.uni.na3.javafx.editEventScreen;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class EditEventController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private TextField timeField;
+
+    @FXML
+    private TextField dateField;
+
+    @FXML
+    private TextArea descriptionField;
+
+    @FXML
+    void editClicked(ActionEvent event) {
+
+    }
+
+}
