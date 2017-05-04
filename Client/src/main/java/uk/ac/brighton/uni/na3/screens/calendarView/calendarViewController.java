f75c6729cae3485a9c30f73f53d8e69533d3833f
Constantinos Ioannou <conioannou29@gmail.com>
2017-05-04 20:35:27 +0100

    calendarViewController.java

---
+++ Client/src/main/java/uk/ac/brighton/uni/na3/screens/calendarView/calendarViewController.java
@@ -0,0 +1,41 @@
+package uk.ac.brighton.uni.na3.screens.calendarView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Color x232;
+
+    @FXML
+    private Color x231;
+
+    @FXML
+    private Color x23;
+
+    @FXML
+    private Font x12;
+
+    @FXML
+    private Color x22;
+
+    @FXML
+    void initialize() {
+        assert x232 != null : "fx:id=\"x232\" was not injected: check your FXML file 'Calendar_View.fxml'.";
+        assert x231 != null : "fx:id=\"x231\" was not injected: check your FXML file 'Calendar_View.fxml'.";
+        assert x23 != null : "fx:id=\"x23\" was not injected: check your FXML file 'Calendar_View.fxml'.";
+        assert x12 != null : "fx:id=\"x12\" was not injected: check your FXML file 'Calendar_View.fxml'.";
+        assert x22 != null : "fx:id=\"x22\" was not injected: check your FXML file 'Calendar_View.fxml'.";
+
+    }
+}
