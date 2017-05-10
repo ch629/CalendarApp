fecda2a20080f81473782570b283581ed7648441
Lewis-Allen <lraa12@brighton.ac.uk>
2017-05-10 21:42:25 +0100

    Events should be added to table on daily view, will be properly tested when db has data

--- Client/src/main/java/uk/ac/brighton/uni/na3/screens/dayView/DayController.java
+++ Client/src/main/java/uk/ac/brighton/uni/na3/screens/dayView/DayController.java
@@ -2,6 +2,7 @@ package uk.ac.brighton.uni.na3.screens.dayView;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,7 +15,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import uk.ac.brighton.uni.na3.CalendarApp;
 import uk.ac.brighton.uni.na3.ControlledView;
 import uk.ac.brighton.uni.na3.ScreenController;
+import uk.ac.brighton.uni.na3.model.NewEvent;
 import uk.ac.brighton.uni.na3.screens.EventData;
+import uk.ac.brighton.uni.na3.utils.EventUtils;
 
 public class DayController implements ControlledView {
 
@@ -51,7 +54,13 @@ public class DayController implements ControlledView {
 		table.setPlaceholder(new Label(datePicker.getValue().equals(LocalDate.now()) ? "You have no events today."
 				: "You have no events planned for this day."));
 		
-	    ObservableList<EventData> events = table.getItems();
+	    ObservableList<EventData> eventsInTable = table.getItems();
+	    List<NewEvent> eventsToDisplay = EventUtils.getEventsOnDay(datePicker.getValue());
+	    
+	    for( NewEvent n : eventsToDisplay){
+	    	EventData event = new EventData(n.getDescription(), LocalTime.now(), n.getDescription(), n.getDescription(), n.getLocation() );
+	    	eventsInTable.add(event);
+	    }
 	    
 	    //TODO: Grab events from server and insert into table.getItems()
 	}
