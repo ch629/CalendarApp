bedba9ad4662c635dfa4cd0982f992cde877d243
Charlie Howes <Zurrox@live.com>
2017-05-12 00:27:44 +0100

    Removed old generic.

--- Common/src/main/java/uk/ac/brighton/uni/na3/model/networking/request/event/EventOnDayRequest.java
+++ Common/src/main/java/uk/ac/brighton/uni/na3/model/networking/request/event/EventOnDayRequest.java
@@ -1,23 +1,12 @@
 package uk.ac.brighton.uni.na3.model.networking.request.event;
 
-import uk.ac.brighton.uni.na3.model.Event;
 import uk.ac.brighton.uni.na3.model.networking.request.Request;
-import uk.ac.brighton.uni.na3.model.networking.response.SingleDataResponse;
 
-import java.util.List;
-
-public class EventOnDayRequest extends Request<SingleDataResponse<List<Event>>> {
+public class EventOnDayRequest extends Request {
     private int day, month, year;
 
     public EventOnDayRequest(char[] authToken, int day, int month, int year) {
         super(authToken);
-        this.day = day;
-        this.month = month;
-        this.year = year;
-    }
-
-    public EventOnDayRequest(char[] authToken, SingleDataResponse<List<Event>> expectedResponse, int day, int month, int year) {
-        super(authToken, expectedResponse);
         this.day = day;
         this.month = month;
         this.year = year;
